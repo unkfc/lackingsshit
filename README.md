b83f723c60fe11af849409071aa79a00910d3a97
unkfc <76722171+unkfc@users.noreply.github.com>
2021-04-30 11:04:08 -0600

    Add files via upload

---
+++ README.md
@@ -0,0 +1,65 @@
+# discord-uptime
+[![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/psf/black)
+[![security: bandit](https://img.shields.io/badge/security-bandit-yellow.svg)](https://github.com/PyCQA/bandit)
+
+Discord Uptime is a Discord bot that allows you to monitor the uptime of services using ICMP ping and http requests. 
+There are also commands avaliable to make manual requests. Built using discord.py 1.6.x, ping3 and aiohttp
+
+## Installation
+**Requires Python 3.6+**
+
+Install dependencies: 
+
+`pip install -r requirements.txt`
+
+Bot setup (Rename config.example.json and edit the default values):
+* `token` - Discord bot token
+* `prefix` - Discord bot prefix
+* `activity_type` - Activity type for bot status (must be one of `playing, streaming, listening, watching`)
+* `activity_name` - Text for bot status
+* `disable_help` - If true, the default help command will be disabled
+* `hide_ips` - If true, any IP addresses in notifications or the status command will be hidden
+* `notification_channel` - Channel ID where down/up notifications will be sent
+* `role_to_mention` - Role ID which will be tagged in down/up notifications
+* `secs_between_ping` - How many seconds between each uptime check
+* `timeout` - How many seconds before a ping or HTTP request should timeout
+
+No privileged intents are currently needed to run the bot.
+
+## Servers Configuration
+Servers should be setup similar to the examples already in `server.json`:
+* There are two supported types: `http` and `ping`
+```json
+[
+    {
+      "name": "Google",
+      "type": "http",
+      "address": "google.com"
+    },
+    {
+      "name": "Cloudflare",
+      "type": "ping",
+      "address": "1.1.1.1"
+    }
+]
+```
+
+## Commands
+> Default Prefix: >
+
+* `ping <address> [pings]` - Pings an address once, or for the amount specified via pings and returns the delay in ms
+* `http <address>` - Performs a HTTP request to the specified address and returns the response code
+* `status` - Displays the status of all servers setup in `servers.json`
+
+## Screenshots
+> Status Command
+
+![status](https://i.gyazo.com/6d5e0c4fbdb5ff52619d86eef827e369.png)
+
+> Uptime & Downtime Notifications
+
+![uptime](https://i.gyazo.com/803aebfcb3833ac8de7bd38e18378a29.png)
+
+> Hide IP addresses
+
+![hideips](https://i.gyazo.com/8596a75d33aa85716ba86f7e01621bb1.png)
---
+++ cogs/monitor.py
@@ -0,0 +1,131 @@
+import asyncio
+from datetime import timedelta
+
+import aiohttp
+import discord
+from discord.ext import tasks, commands
+from ping3 import ping
+
+from utils import embeds
+from utils.config import get_config, get_servers, get_server_name
+
+
+class Monitor(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.currently_down = {}
+        self.monitor_uptime.start()
+
+    def cog_unload(self):
+        self.monitor_uptime.cancel()
+
+    async def notify_down(
+        self, server: object, channel: discord.TextChannel, reason: str
+    ) -> None:
+        """
+        Sends an embed to indicate a service is offline
+        :param server: Server object to extract data from
+        :param channel: Channel to send the notification to
+        :param reason: Reason why the service is down
+        """
+        if server["address"] not in self.currently_down:
+            self.currently_down.update({server["address"]: 0})
+            embed = embeds.Embed(
+                title=f"**:red_circle: {get_server_name(server['address'])} is down!**",
+                color=16711680,
+            )
+            embed.add_field(name="Address", value=server["address"], inline=False)
+            embed.add_field(name="Type", value=server["type"], inline=False)
+            embed.add_field(name="Reason", value=reason, inline=False)
+            await channel.send(embed=embed)
+            await channel.send(f"<@&{get_config('role_to_mention')}>", delete_after=3)
+        else:
+            self.currently_down[server["address"]] = self.currently_down.get(
+                server["address"], 0
+            ) + get_config("secs_between_ping")
+
+    async def notify_up(self, server: object, channel: discord.TextChannel) -> None:
+        """
+        Sends an embed to indicate a service is online
+        :param server: Server object to extract data from
+        :param channel: Channel to send the notification to
+        """
+        if server["address"] in self.currently_down:
+            embed = embeds.Embed(
+                title=f"**:green_circle: {get_server_name(server['address'])} is up!**",
+                color=65287,
+            )
+            embed.add_field(name="Address", value=server["address"], inline=False)
+            embed.add_field(name="Type", value=server["type"], inline=False)
+            embed.add_field(
+                name="Downtime",
+                value=str(timedelta(seconds=self.currently_down[server["address"]])),
+                inline=False,
+            )
+            await channel.send(embed=embed)
+            await channel.send(f"<@&{get_config('role_to_mention')}>", delete_after=3)
+            self.currently_down.pop(server["address"])
+
+    @tasks.loop(seconds=get_config("secs_between_ping"))
+    async def monitor_uptime(self) -> None:
+        """Checks the status of each server and sends up/down notifications"""
+        await self.bot.wait_until_ready()
+
+        channel = self.bot.get_channel(get_config("notification_channel"))
+        timeout = get_config("timeout")
+
+        for i in get_servers():
+            if i["type"] == "ping":
+                if ping(i["address"], timeout=timeout) is False:
+                    await self.notify_down(i, channel, "Host unknown")
+                elif ping(i["address"], timeout=timeout) is None:
+                    await self.notify_down(i, channel, "Timed out")
+                else:
+                    await self.notify_up(i, channel)
+            else:
+                address = i["address"]
+
+                if not address.startswith("http"):
+                    address = f"http://{address}"
+
+                async with aiohttp.ClientSession(
+                    timeout=aiohttp.ClientTimeout(total=timeout)
+                ) as session:
+                    try:
+                        async with session.get(address) as res:
+                            if res.ok:
+                                await self.notify_up(i, channel)
+                            else:
+                                await self.notify_down(i, channel, res.reason)
+                    except asyncio.TimeoutError:
+                        await self.notify_down(i, channel, "Timed out")
+                    except aiohttp.ClientError:
+                        await self.notify_down(i, channel, "Connection failed")
+
+    @commands.command(brief="Checks status of servers being monitored", usage="status")
+    async def status(self, ctx) -> None:
+        """Returns an embed showing the status of each monitored server"""
+        embed = embeds.Embed(
+            title="**Monitor Status**", color=16711680 if self.currently_down else 65287
+        )
+
+        for i in get_servers():
+            if i["address"] in self.currently_down:
+                downtime = str(timedelta(seconds=self.currently_down[i["address"]]))
+                embed.add_field(
+                    name=f"{i['name']} ({i['type']})",
+                    value=f":red_circle: {i['address']} ({downtime})",
+                    inline=False,
+                )
+            else:
+                embed.add_field(
+                    name=f"{i['name']} ({i['type']})",
+                    value=f":green_circle: {i['address']}",
+                    inline=False,
+                )
+
+        await ctx.send(embed=embed)
+
+
+def setup(bot):
+    bot.add_cog(Monitor(bot))
